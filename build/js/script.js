d0f7209d3a2c62ee8ddf61d77a34b0d2e76ca8e8
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-03-05 11:57:32 +0200

    Подключает в попап js

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var backspaceKeyCode = 8;
+var KeyCode = {
+  BACKSPACE: 8,
+  ESC: 27,
+};
 
 // footer
 (function () {
@@ -63,84 +66,123 @@ var backspaceKeyCode = 8;
 
 // questions
 (function () {
-  var form = document.querySelector('.questions__inner form');
-  var questionsPhone = form.querySelector('.questions__phone');
-  var phoneInput = questionsPhone.querySelector('input');
-  var submitBtn = form.querySelector('button[type="submit"]');
+  var validateQuestions = function (form) {
+    var questionsPhone = form.querySelector('.questions__phone');
+    var phoneInput = questionsPhone.querySelector('input');
+    var submitBtn = form.querySelector('button[type="submit"]');
 
-  var validatePhone = function () {
-    var string = phoneInput.value;
-    var result = string.match(/\+7\(\d{3}\)\d{7}/);
-    var foundMatch = result ? result[0] : null;
-    if (foundMatch === string) {
-      if (questionsPhone.classList.contains('questions__phone--no-match')) {
-        questionsPhone.classList.remove('questions__phone--no-match');
-        phoneInput.setCustomValidity('');
-      }
-    } else {
-      questionsPhone.classList.add('questions__phone--no-match');
-      phoneInput.setCustomValidity('Номер телефона должен соответствовать следующий маске +7(000)0000000');
-    }
-  };
-
-  var subscribePhoneEvents = function () {
-    var findStringMatch = function (matchResult) {
-      return matchResult ? matchResult[0] : null;
-    };
-    phoneInput.addEventListener('focus', function () {
-      if (phoneInput.value === '') {
-        phoneInput.value = '+7(';
-      }
-    });
-
-    phoneInput.addEventListener('change', function () {
-      validatePhone();
-    });
-
-    phoneInput.addEventListener('keyup', function (evt) {
+    var validatePhone = function () {
       var string = phoneInput.value;
-      if (evt.keyCode === backspaceKeyCode &&
-        string.match(/\+7\(\d{3}/) &&
-        !string.match(/\+7\(\d{3}\)\d*/)) {
-        phoneInput.value = string.slice(0, -1);
-        return;
-      }
-
-      if (findStringMatch(string.match(/\+7\(\d{3}/)) === string) {
-        phoneInput.value = string + ')';
-      }
-
-      if (findStringMatch(string.match(/(\+7\((\d){3}\)?(\d){0,7})|\+7\((\d){0,3}/)) === string) {
+      var result = string.match(/\+7\(\d{3}\)\d{7}/);
+      var foundMatch = result ? result[0] : null;
+      if (foundMatch === string) {
         if (questionsPhone.classList.contains('questions__phone--no-match')) {
           questionsPhone.classList.remove('questions__phone--no-match');
+          phoneInput.setCustomValidity('');
         }
       } else {
         questionsPhone.classList.add('questions__phone--no-match');
         phoneInput.setCustomValidity('Номер телефона должен соответствовать следующий маске +7(000)0000000');
       }
+    };
+
+    var subscribePhoneEvents = function () {
+      var findStringMatch = function (matchResult) {
+        return matchResult ? matchResult[0] : null;
+      };
+      phoneInput.addEventListener('focus', function () {
+        if (phoneInput.value === '') {
+          phoneInput.value = '+7(';
+        }
+      });
+
+      phoneInput.addEventListener('change', function () {
+        validatePhone();
+      });
+
+      phoneInput.addEventListener('keyup', function (evt) {
+        var string = phoneInput.value;
+        if (evt.keyCode === KeyCode.BACKSPACE &&
+          string.match(/\+7\(\d{3}/) &&
+          !string.match(/\+7\(\d{3}\)\d*/)) {
+          phoneInput.value = string.slice(0, -1);
+          return;
+        }
+
+        if (findStringMatch(string.match(/\+7\(\d{3}/)) === string) {
+          phoneInput.value = string + ')';
+        }
+
+        if (findStringMatch(string.match(/(\+7\((\d){3}\)?(\d){0,7})|\+7\((\d){0,3}/)) === string) {
+          if (questionsPhone.classList.contains('questions__phone--no-match')) {
+            questionsPhone.classList.remove('questions__phone--no-match');
+          }
+        } else {
+          questionsPhone.classList.add('questions__phone--no-match');
+          phoneInput.setCustomValidity('Номер телефона должен соответствовать следующий маске +7(000)0000000');
+        }
+      });
+    };
+
+    subscribePhoneEvents();
+    submitBtn.addEventListener('click', function (evt) {
+      validatePhone();
+      if (form.checkValidity()) {
+        evt.preventDefault();
+      }
     });
   };
-
-  subscribePhoneEvents();
-  submitBtn.addEventListener('click', function (evt) {
-    validatePhone();
-    if (form.checkValidity()) {
-      evt.preventDefault();
-    }
+  var formCollection = document.querySelectorAll('.questions__inner form');
+  formCollection.forEach(function (it) {
+    validateQuestions(it);
   });
 })();
 
-var anchors = document.querySelectorAll('a.scroll-to');
+// popup
+(function () {
+  var showBtn = document.querySelector('.main-nav__recall a');
+  var popup = document.querySelector('.questions--popup');
+  var form = popup.querySelector('form');
+  var closeBtn = form.querySelector('.questions__close');
 
-anchors.forEach(function (anchor) {
-  anchor.addEventListener('click', function (evt) {
+  var onDocumentPopupEscKeyDown = function (evt) {
+    if (evt.keyCode === KeyCode.ESC) {
+      closePopup();
+    }
+  };
+  var showPopup = function () {
+    popup.style.display = 'flex';
+    document.addEventListener('keydown', onDocumentPopupEscKeyDown);
+  };
+  var closePopup = function () {
+    popup.style.display = 'none';
+    document.removeEventListener('keydown', onDocumentPopupEscKeyDown);
+  };
+  closeBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
+    closePopup();
+  });
+  showBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    showPopup();
+  });
 
-    var blockID = anchor.getAttribute('href');
+})();
 
-    document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+// smooth scroll
+(function () {
+  var anchors = document.querySelectorAll('a.scroll-to');
+
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      var blockID = anchor.getAttribute('href');
+
+      document.querySelector(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
     });
   });
-});
+})();
--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var backspaceKeyCode = 8;
+var KeyCode = {
+  BACKSPACE: 8,
+  ESC: 27,
+};
 
 // footer
 (function () {
@@ -63,84 +66,138 @@ var backspaceKeyCode = 8;
 
 // questions
 (function () {
-  var form = document.querySelector('.questions__inner form');
-  var questionsPhone = form.querySelector('.questions__phone');
-  var phoneInput = questionsPhone.querySelector('input');
-  var submitBtn = form.querySelector('button[type="submit"]');
+  var validateQuestions = function (form) {
+    var questionsPhone = form.querySelector('.questions__phone');
+    var phoneInput = questionsPhone.querySelector('input');
+    var submitBtn = form.querySelector('button[type="submit"]');
 
-  var validatePhone = function () {
-    var string = phoneInput.value;
-    var result = string.match(/\+7\(\d{3}\)\d{7}/);
-    var foundMatch = result ? result[0] : null;
-    if (foundMatch === string) {
-      if (questionsPhone.classList.contains('questions__phone--no-match')) {
-        questionsPhone.classList.remove('questions__phone--no-match');
-        phoneInput.setCustomValidity('');
-      }
-    } else {
-      questionsPhone.classList.add('questions__phone--no-match');
-      phoneInput.setCustomValidity('Номер телефона должен соответствовать следующий маске +7(000)0000000');
-    }
-  };
-
-  var subscribePhoneEvents = function () {
-    var findStringMatch = function (matchResult) {
-      return matchResult ? matchResult[0] : null;
-    };
-    phoneInput.addEventListener('focus', function () {
-      if (phoneInput.value === '') {
-        phoneInput.value = '+7(';
-      }
-    });
-
-    phoneInput.addEventListener('change', function () {
-      validatePhone();
-    });
-
-    phoneInput.addEventListener('keyup', function (evt) {
+    var validatePhone = function () {
       var string = phoneInput.value;
-      if (evt.keyCode === backspaceKeyCode &&
-        string.match(/\+7\(\d{3}/) &&
-        !string.match(/\+7\(\d{3}\)\d*/)) {
-        phoneInput.value = string.slice(0, -1);
-        return;
-      }
-
-      if (findStringMatch(string.match(/\+7\(\d{3}/)) === string) {
-        phoneInput.value = string + ')';
-      }
-
-      if (findStringMatch(string.match(/(\+7\((\d){3}\)?(\d){0,7})|\+7\((\d){0,3}/)) === string) {
+      var result = string.match(/\+7\(\d{3}\)\d{7}/);
+      var foundMatch = result ? result[0] : null;
+      if (foundMatch === string) {
         if (questionsPhone.classList.contains('questions__phone--no-match')) {
           questionsPhone.classList.remove('questions__phone--no-match');
+          phoneInput.setCustomValidity('');
         }
       } else {
         questionsPhone.classList.add('questions__phone--no-match');
         phoneInput.setCustomValidity('Номер телефона должен соответствовать следующий маске +7(000)0000000');
       }
+    };
+
+    var subscribePhoneEvents = function () {
+      var findStringMatch = function (matchResult) {
+        return matchResult ? matchResult[0] : null;
+      };
+      phoneInput.addEventListener('focus', function () {
+        if (phoneInput.value === '') {
+          phoneInput.value = '+7(';
+        }
+      });
+
+      phoneInput.addEventListener('change', function () {
+        validatePhone();
+      });
+
+      phoneInput.addEventListener('keyup', function (evt) {
+        var string = phoneInput.value;
+        if (evt.keyCode === KeyCode.BACKSPACE &&
+          string.match(/\+7\(\d{3}/) &&
+          !string.match(/\+7\(\d{3}\)\d*/)) {
+          phoneInput.value = string.slice(0, -1);
+          return;
+        }
+
+        if (findStringMatch(string.match(/\+7\(\d{3}/)) === string) {
+          phoneInput.value = string + ')';
+        }
+
+        if (findStringMatch(string.match(/(\+7\((\d){3}\)?(\d){0,7})|\+7\((\d){0,3}/)) === string) {
+          if (questionsPhone.classList.contains('questions__phone--no-match')) {
+            questionsPhone.classList.remove('questions__phone--no-match');
+          }
+        } else {
+          questionsPhone.classList.add('questions__phone--no-match');
+          phoneInput.setCustomValidity('Номер телефона должен соответствовать следующий маске +7(000)0000000');
+        }
+      });
+    };
+
+    subscribePhoneEvents();
+    submitBtn.addEventListener('click', function (evt) {
+      validatePhone();
+      if (form.checkValidity()) {
+        evt.preventDefault();
+      }
     });
   };
+  var form = document.querySelector('main .questions__inner form');
+  validateQuestions(form);
 
-  subscribePhoneEvents();
-  submitBtn.addEventListener('click', function (evt) {
-    validatePhone();
-    if (form.checkValidity()) {
-      evt.preventDefault();
-    }
-  });
+  window.validateQuestions = validateQuestions;
 })();
 
-var anchors = document.querySelectorAll('a.scroll-to');
+// popup
+(function () {
+  var showBtn = document.querySelector('.main-nav__recall a');
 
-anchors.forEach(function (anchor) {
-  anchor.addEventListener('click', function (evt) {
+  var popupTemplate = document.querySelector('#questions-popup').content.querySelector('.questions--popup');
+  var addPopup = function () {
+    var element = popupTemplate.cloneNode(true);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    return document.body.querySelector('.questions--popup');
+  };
+  var popup = addPopup();
+  var form = popup.querySelector('form');
+  var closeBtn = popup.querySelector('.questions__close');
+  var onDocumentPopupEscKeyDown = function (evt) {
+    if (evt.keyCode === KeyCode.ESC) {
+      closePopup();
+    }
+  };
+  var onClickOutsideForm = function (evt) {
+    if (evt.target.classList.contains('questions--popup')) {
+      closePopup();
+    }
+  };
+  var showPopup = function () {
+    popup.style.display = 'flex';
+    document.addEventListener('keydown', onDocumentPopupEscKeyDown);
+    window.addEventListener('click', onClickOutsideForm);
+  };
+  var closePopup = function () {
+    popup.style.display = 'none';
+    document.removeEventListener('keydown', onDocumentPopupEscKeyDown);
+    window.removeEventListener('click', onClickOutsideForm);
+  };
+  window.validateQuestions(form);
+  closeBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
+    closePopup();
+  });
+  showBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    showPopup();
+  });
 
-    var blockID = anchor.getAttribute('href');
+})();
 
-    document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+// smooth scroll
+(function () {
+  var anchors = document.querySelectorAll('a.scroll-to');
+
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      var blockID = anchor.getAttribute('href');
+
+      document.querySelector(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
     });
   });
-});
+})();
