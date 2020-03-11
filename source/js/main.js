acb5bf97a2410f5b1d8c48dbef6ea667dac17b03
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-03-11 16:54:47 +0200

    Заменяет template обычным блоком, для совместимости с IE11

--- source/js/main.js
+++ source/js/main.js
@@ -4,17 +4,10 @@ import MainQuestionsSection from './main-questions-section';
 import PopupQuestionsSection from './popup-questions-section';
 
 const showBtn = document.querySelector(`.main-nav__recall a`);
-const addPopup = function () {
-  const popupTemplate = document.querySelector(`#questions-popup`).content.querySelector(`.questions--popup`);
-  const element = popupTemplate.cloneNode(true);
-  element.style.display = `none`;
-  document.body.appendChild(element);
-  return document.body.querySelector(`.questions--popup`);
-};
 
 activeSmoothScroll();
 activeFooter();
 const mainQuestionsSection = new MainQuestionsSection(document.querySelector(`main .questions`));
 mainQuestionsSection.validateQuestions();
-const popupQuestionsSection = new PopupQuestionsSection(addPopup(), showBtn);
+const popupQuestionsSection = new PopupQuestionsSection(document.querySelector(`body > .questions`), showBtn);
 popupQuestionsSection.validateQuestions();
